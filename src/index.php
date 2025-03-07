8bcf9bc75f5f78528e7054b50a585328d14c10cf
Rimsha <rimiqbal777@gmail.com>
2025-03-07 09:56:38 +0000

    m

--- src/index.php
+++ src/index.php
@@ -1,7 +1,11 @@
 <?php
+/*Incluye parámetros de conexión a la base de datos: 
+DB_HOST: Nombre o dirección del gestor de BD MariaDB
+DB_NAME: Nombre de la BD
+DB_USER: Usuario de la BD
+DB_PASSWORD: Contraseña del usuario de la BD
+*/
 include_once("config.php");
-
-$resultado = $mysqli->query("SELECT * FROM servicios_eventos ORDER BY tipo_evento, nombre_servicio");
 ?>
 
 <!DOCTYPE html>
@@ -10,6 +14,8 @@ $resultado = $mysqli->query("SELECT * FROM servicios_eventos ORDER BY tipo_event
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">	
 	<title>Decoraciones Con Rimsha</title>
+	
+		
 </head>
 <body>
 <div>
@@ -33,12 +39,15 @@ $resultado = $mysqli->query("SELECT * FROM servicios_eventos ORDER BY tipo_event
 					<th>Precio Estimado</th>
 					<th>Descripcion</th>
 					<th>Fecha Creacion</th>
-					<th>Acciones</th>
 				</tr>
 			</thead>
 			<tbody>
 
 <?php
+$resultado = $mysqli->query("SELECT * FROM servicios_eventos ORDER BY tipo_evento, nombre_servicio");
+
+$mysqli->close();
+
 if ($resultado->num_rows > 0) {
 	while($fila = $resultado->fetch_array()) {
 		echo "<tr>\n";
@@ -55,11 +64,7 @@ if ($resultado->num_rows > 0) {
 		echo "</td>";
 		echo "</tr>\n";
 	}
-} else {
-    echo "<tr><td colspan='8'>No hay servicios disponibles.</td></tr>";
 }
-
-$mysqli->close(); // Close after fetching results
 ?>
 			</tbody>
 		</table>
